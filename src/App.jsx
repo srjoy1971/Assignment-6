778ee68f692d096e57403bacf3dc2f3a1dabb4f6
srjoy1971 <srjoy406@gmail.com>
2026-03-30 17:46:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navber from "./Component/Navber"
+
+function App() {
+
+  return (
+    <>
+      < Navber> </Navber>
+      <h1 className="">hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Navber.jsx
@@ -0,0 +1,10 @@
+const Navber = () => {
+  return (
+    <div>
+      Navber
+      <h1 className="text"></h1>
+    </div>
+  );
+};
+
+export default Navber;
